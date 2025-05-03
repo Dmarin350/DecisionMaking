3faddda94abb4bc6fdaa31684fcafb1f72ebc6aa
Dmarin350 <djmarin1@miners.utep.edu>
2025-05-03 13:46:28 -0600

    Value iteration PA3 added

---
+++ Class_Project/main.py
@@ -0,0 +1,4 @@
+import pandas as pd
+# df = pd.read_csv('/kaggle/input/disease-symptoms-and-patient-profile-dataset/Disease_symptom_and_patient_profile_dataset.csv')
+df = pd.read_csv('/Users/danielmarin/PA2_Decision_Making/DecisionMaking/Class_Project/patient_dataset.csv')
+df.head()
